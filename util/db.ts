46c067bacaea917f509ffee2b34be045d3045f14
myFunnyVa1entine <brilliant2102@gmail.com>
2024-04-03 21:25:55 +0900

    DEL : remove pakage dotenv

--- util/db.ts
+++ util/db.ts
@@ -1,26 +1,24 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
-// import 'dotenv/config';
-require('dotenv').config();
 
-const uri = process.env.MONGODB_URI!
-const DB_NAME = "paper_ground";
+const uri = process.env.MONGODB_URI!;
+const DB_NAME = 'paper_ground';
 const COLLECTION_NAME = 'artist_info';
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
 });
 
 export default async function ConnectDB() {
-  try {
-    await client.connect();
-    const db = client.db(DB_NAME);
+	try {
+		await client.connect();
+		const db = client.db(DB_NAME);
 		const collection = db.collection(COLLECTION_NAME);
-    return collection;
-  } catch (err) {
-    console.error(err);
-  }
+		return collection;
+	} catch (err) {
+		console.error(err);
+	}
 }
